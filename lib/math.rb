dd983f923aab7f30fddd0cdf5cc5020857dd2d36
Amer Fahmy <afahmy519@gmail.com>
2020-04-24 14:43:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  puts num2 - num1
 
 end
 
 def division(num1, num2)
+ puts num2 / num1 
 
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 
 end
 
 def modulo(num1, num2)
+  puts 
 
 end
 
 def square_root(num)
+  
 
 end
